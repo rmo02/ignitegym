a26d4aa4ed8edb45aadeb507057f78f6daa78b3a
rmo02 <ramoon.maia23@gmail.com>
2023-02-02 11:03:57 -0300

    autenticação finalizada

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,11 @@
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
 import {
+  storageAuthTokenGet,
+  storageAuthTokenRemove,
+  storageAuthTokenSave,
+} from "@storage/storageAuthToken";
+import {
   storageUserGet,
   storageUserRemove,
   storageUserSave,
@@ -10,7 +15,7 @@ import { createContext, ReactNode, useEffect, useState } from "react";
 export type AuthContextDataProps = {
   user: UserDTO;
   singIn: (email: string, password: string) => Promise<void>;
-  singOut: () => Promise <void>;
+  singOut: () => Promise<void>;
   isLoadingUserStorageData: boolean;
 };
 
@@ -27,16 +32,37 @@ export function AuthContextProvider({ children }: AuthContexteProviderProps) {
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
     useState(true);
 
+  async function userAndTokenUpdate(userData: UserDTO, token: string) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+    setUser(userData);
+  }
+
+  async function storageUserAndTokenSave(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true);
+
+      await storageUserSave(userData);
+      await storageAuthTokenSave(token);
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
   async function singIn(email: string, password: string) {
     try {
       const { data } = await api.post("/sessions", { email, password });
 
-      if (data.user) {
-        setUser(data.user);
-        storageUserSave(data.user);
+      if (data.user && data.token) {
+        await storageUserAndTokenSave(data.user, data.token);
+        userAndTokenUpdate(data.user, data.token);
       }
     } catch (error) {
       throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
     }
   }
 
@@ -45,6 +71,7 @@ export function AuthContextProvider({ children }: AuthContexteProviderProps) {
       setIsLoadingUserStorageData(true);
       setUser({} as UserDTO);
       await storageUserRemove();
+      await storageAuthTokenRemove();
     } catch (error) {
       throw error;
     } finally {
@@ -54,11 +81,13 @@ export function AuthContextProvider({ children }: AuthContexteProviderProps) {
 
   async function loadUserData() {
     try {
-      const userLogged = await storageUserGet();
+      setIsLoadingUserStorageData(true);
 
-      if (userLogged) {
-        setUser(userLogged);
-        setIsLoadingUserStorageData(false);
+      const userLogged = await storageUserGet();
+      const token = await storageAuthTokenGet();
+
+      if (token && userLogged) {
+        userAndTokenUpdate(userLogged, token);
       }
     } catch (error) {
       throw error;
@@ -72,7 +101,9 @@ export function AuthContextProvider({ children }: AuthContexteProviderProps) {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, singIn, isLoadingUserStorageData, singOut }}>
+    <AuthContext.Provider
+      value={{ user, singIn, isLoadingUserStorageData, singOut }}
+    >
       {children}
     </AuthContext.Provider>
   );
--- src/screens/SingUp.tsx
+++ src/screens/SingUp.tsx
@@ -178,3 +178,4 @@ export function SingUp() {
     </ScrollView>
   );
 }
+ 
